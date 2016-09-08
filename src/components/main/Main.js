2a2c115e1a3f5e11feb7930aceceb91b815aaece
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2016-09-08 08:45:07 +0200

    jquery auth call

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import $ from 'jquery';
 
-import FlowsList from './../flows/FlowsList';
+// import FlowsList from './../flows/FlowsList';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="main">
-        <FlowsList name="default" flows={['prova']}/>
+        {
+          /* <FlowsList name="default" flows={['prova']}/> */
+        }
+        <button onClick={this.login}>Github Login</button>
       </div>
     );
+  }
+
+  login() {
+    console.log('LOGIN');
+    $.get({
+      dataType: 'jsonp',
+      url: 'http://localhost:5000/github-auth'
+    }).done(function(data) {
+      console.log('AUTH DONE', data);
+    })
+      // .then(function(data) {
+      //   console.log(data);
+      // })
   }
 }
 
